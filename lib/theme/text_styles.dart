befc4b3ed15f8ced68699c005fbaeac75a94335e
prudhviraj <sprudhviraj17@gmail.com>
2025-04-11 11:25:20 +0530

    test

--- lib/theme/text_styles.dart
+++ lib/theme/text_styles.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+//Color
 class AppTextStyles {
   static const title = TextStyle(fontSize: 18, fontWeight: FontWeight.w600);
   static const subtitle = TextStyle(fontSize: 15, color: Colors.grey);
